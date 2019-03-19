db3ab2d57c5ccd2f024f409c9050e97a4e35185c
Lennon <lennon@LennondeMacBook-Pro.local>
2019-03-19 14:08:06 +0800

    add commit

--- project/alien_invasion/game_functions.py
+++ project/alien_invasion/game_functions.py
@@ -56,4 +56,3 @@ def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
-
